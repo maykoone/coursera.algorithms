d5e23d7ebd191d189c35bb309d91ead69c684feb
Mayko B. Oliveira <maykoone@gmail.com>
2017-10-08 00:07:52 -0300

    Added test cases

--- union-find/src/test/java/algorithms/UnionFindTest.java
+++ union-find/src/test/java/algorithms/UnionFindTest.java
@@ -1,15 +1,17 @@
 package algorithms;
 
-import org.junit.Assert;
-import org.junit.BeforeClass;
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.*;
 import java.util.Scanner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 
 /**
  * Created by maykoone on 02/10/17.
@@ -22,7 +24,7 @@ public abstract class UnionFindTest {
     public static final int NUMBER_OF_OBJECTS = 10;
 
     //    @BeforeClass
-    public static void setup() throws IOException {
+    public static void setupClass() throws IOException {
         log.info("Read file {}", FILE_NAME);
         Scanner scanner = new Scanner(UnionFind.class.getClassLoader().getResourceAsStream(FILE_NAME));
         int n = scanner.nextInt();
@@ -34,32 +36,57 @@ public abstract class UnionFindTest {
         }
     }
 
+
+    @Before
+    public void setup() {
+        subject = createInstanceWithNumberOfObjects(NUMBER_OF_OBJECTS);
+    }
+
     protected abstract UnionFind createInstanceWithNumberOfObjects(int n);
 
 
     @Test
     public void shouldBeInitializedWithNumberOfObjects() {
-        subject = createInstanceWithNumberOfObjects(NUMBER_OF_OBJECTS);
-        Assert.assertEquals(NUMBER_OF_OBJECTS, subject.size());
+        assertEquals(NUMBER_OF_OBJECTS, subject.size());
     }
 
     @Test
     public void shouldBeConnectedAfterUnion() {
-        subject = createInstanceWithNumberOfObjects(NUMBER_OF_OBJECTS);
-
         subject.union(6, 1);
 
-        Assert.assertTrue(subject.connected(6, 1));
+        assertTrue(subject.connected(6, 1));
     }
 
     @Test
     public void shouldNotBeConnected(){
-        subject = createInstanceWithNumberOfObjects(NUMBER_OF_OBJECTS);
-
-        Assert.assertFalse(subject.connected(6, 1));
+        assertFalse(subject.connected(6, 1));
     }
 
-    
+    @Test
+    public void shouldBeReflexive(){
+        assertTrue(subject.connected(1, 1));
+    }
 
+    @Test
+    public void shouldBeSymmetric() {
+        int p = 5;
+        int q = 0;
+        subject.union(p, q);
+
+        assertTrue(subject.connected(p, q)
+                    && subject.connected(q, p));
+    }
+
+    @Test
+    public void shouldBeTransitive() {
+        int p = 3;
+        int q = 7;
+        int r = 2;
+
+        subject.union(p, q);
+        subject.union(q, r);
+
+        assertTrue(subject.connected(p, r));
+    }
 
 }
